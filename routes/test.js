b9fb1cd4d8e59299d072bd8a1f67a0cb5c61ea87
AdrianPlesner <Adrian.plesner@gmail.com>
2021-10-21 14:28:13 +0200

    Now we use the variable

--- routes/test.js
+++ routes/test.js
@@ -1,11 +1,33 @@
 var express = require('express');
 var router = express.Router();
+let script = require('../app')
+const {spawn} = require("child_process");
 
 
 router.get('/date', function (req, res) {
     let a = new Date(Date.now())
     a.setDate(a.getDate() - 1)
     res.send(JSON.stringify({"date": Date.now()}))
+})
+
+router.get('/path', function (req, res) {
+    const ls = spawn('python', [script.pythonPath + 'test.py', JSON.stringify(req.body)])
+    var result = ""
+    ls.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+        result += (`stdout: ${data}`);
+    });
+
+    ls.stderr.on('data', (data) => {
+        result += (`stderr: ${data}`);
+        console.log(`stderr: ${data}`);
+    });
+
+    ls.on('close', (code) =>{
+        result += (`child proccess exited with code ${code}`)
+        console.log(`child proccess exited with code ${code}`)
+    })
+    res.send(result)
 })
 
 module.exports = router;
