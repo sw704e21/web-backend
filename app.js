6d2a2f8ea065f7e557ca56125f83afa51cea390c
AdrianPlesner <Adrian.plesner@gmail.com>
2021-10-21 14:57:51 +0200

    Different slashes

--- app.js
+++ app.js
@@ -72,5 +72,5 @@ app.listen(3001, () => {
 })
 
 var dir = __dirname;
-module.exports.pythonPath = dir.substr(0, dir.length -'web-backend/'.length) + "\\server-backend\\"
+module.exports.pythonPath = dir.substr(0, dir.length -'web-backend/'.length) + "//server-backend"
 module.exports = app;
