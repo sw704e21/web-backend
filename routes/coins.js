cbf2345823099ac6b73ae6b50f08f02aaf816686
AdrianPlesner <Adrian.plesner@gmail.com>
2021-10-28 14:24:50 +0200

    Added /track/name-identifier endpoint. It checks if name or identifier already exist. If not, create a new one

--- routes/coins.js
+++ routes/coins.js
@@ -1,11 +1,13 @@
 let express = require('express');
 let router = express.Router();
-let Sentiment = require('./models/Sentiment')
-let corsOptions = require('../app').corsOptions
+let Sentiment = require('../models/Sentiment')
+let Coin = require('../models/Coin')
+let app = require('../app')
 let cors = require('cors')
+const {spawn} = require("child_process");
 
 
-router.get('/',cors(corsOptions) , async function (req, res, next) {
+router.get('/',cors(app.corsOptions) , async function (req, res, next) {
     let twoday = new Date(Date.now() - 1000 * 60 * 60 * 24 * 2); // subtract two day
     let oneday = new Date(Date.now() - 1000 * 60 * 60 * 24 ); // subtract one day
     let q = Sentiment.Sentiment.aggregate()
@@ -95,7 +97,20 @@ router.get('/',cors(corsOptions) , async function (req, res, next) {
     });
 });
 
-router.get('/:name', cors(corsOptions), async function (req, res, next) {
+router.post('/', cors(app.corsOptions), async function (req, res) {
+    let body = req.body
+    await Sentiment.Sentiment.create(body, function (err, obj, next) {
+        if (err) {
+            next(err)
+        } else {
+            res.status(201)
+            res.send(obj)
+        }
+    })
+})
+
+
+router.get('/:name', cors(app.corsOptions), async function (req, res, next) {
     let q = Sentiment.Sentiment.find({coin: req.params['name']});
     await q.exec(function (err, result) {
         if (err) {
@@ -112,16 +127,47 @@ router.get('/:name', cors(corsOptions), async function (req, res, next) {
     })
 });
 
-router.post('/', cors(corsOptions), async function (req, res) {
-    const body = req.body
-    await Sentiment.Sentiment.create(body, function (err, obj, next) {
-        if (err) {
-            next(err)
-        } else {
-            res.status(201)
-            res.send(obj)
-        }
-    })
-})
+
+
+router.post('/track/:name-:ident', cors(app.corsOptions), async function(req, res, next){
+   let name = req.params['name'].toLowerCase();
+   let identifier = req.params['ident'].toUpperCase();
+   let q = Coin.Coin.find({$or: [{name: name}, {identifier: identifier}]});
+   await q.exec(async function (err, result) {
+       if (err) {
+           res.status(500)
+           next()
+       } else {
+           if (result.length !== 0) {
+               res.status(409)
+               next("Already tracking a coin with given name or identifier:" + result)
+
+           } else {
+               await Coin.Coin.create({name: name, identifier: identifier}, function (err, obj, next) {
+                   if (err) {
+                       next(err);
+                   } else {
+                       const ls = spawn('python3', [app.crawlerPath + 'src/add_subreddit.py', JSON.stringify(name)])
+                       ls.stdout.on('data', (data) => {
+                           console.log(`stdout: ${data}`);
+                       });
+
+                       ls.stderr.on('data', (data) => {
+                           console.log(`stderr: ${data}`);
+                       });
+
+                       ls.on('close', (code) =>{
+                           console.log(`child proccess exited with code ${code}`)
+                       })
+                       // Start script in crawler
+                       res.status(201)
+                       res.send(obj)
+                   }
+               })
+           }
+       }
+   });
+
+});
 
 module.exports = router;
