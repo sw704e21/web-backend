5dd7555e38e2335666a0f1bcb352e1f0a68eba12
AdrianPlesner <Adrian.plesner@gmail.com>
2021-12-14 10:04:38 +0100

    no print tfdict

--- routes/data.js
+++ routes/data.js
@@ -40,7 +40,6 @@ router.post('/', async function (req, res, next) {
 
 router.post('/tfdict/:identifier', async function(req, res, next){
     let dict = req.body;
-    console.log(dict)
     let ident = req.params['identifier'].toUpperCase();
     let r = Coin.findOne({identifier: ident});
     await r.exec(async function (err, result) {
